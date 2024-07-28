09b72c57948dad6482529b3ae69ed06643f25350
Matheus4ugusto <matheuspaulasoares10@gmail.com>
2024-07-27 21:42:32 -0300

    Configura banco de dados e inicia desenvolvimento das rotas de usuário

---
+++ db/migrations/20240727190140_create-users-table.ts
@@ -0,0 +1,19 @@
+import type { Knex } from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+    await knex.schema.createTable('users', table => {
+        table.string('email').unique().primary()
+        table.string('password').notNullable()
+        table.string('name').nullable()
+        table.uuid('id').unique()
+        table.uuid('authorization_token').notNullable().unique()
+        table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable()
+    })
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+    await knex.schema.dropTable('users')
+}
+
